97889c3e6f85fdd6d189039df27255f9fce2a158
nguye374 <nguye374@sheridancollege.ca>
2021-11-07 03:48:56 -0500

    Nov 7 CSS module

--- src/pages/about.js
+++ src/pages/about.js
@@ -1,10 +1,14 @@
 import React from "react"
 import Layout from "../components/Layout"
+import { page, text } from "../examples/about.module.css"
 
 export default function about() {
   return (
     <Layout>
-      <h1>About Page</h1>
+      <div className={page}>
+        <h1>About Page</h1>
+        <p>Lorem</p>
+      </div>
     </Layout>
   )
 }
--- src/pages/index.js
+++ src/pages/index.js
@@ -1,21 +1,23 @@
 import React from "react"
 import { Link } from "gatsby"
 import Layout from "../components/Layout"
+import { page, text } from "../examples/home.module.css"
 
 export default function Home() {
   return (
     <Layout>
-      <h1 style={{ color: "green", textTransform: "capitalize" }}>
-        Hello page
-      </h1>
-      <div>
-        <Link to="/about">About page</Link>
-      </div>
-      <div>
-        <Link to="/company/history">History page</Link>
-      </div>
-      <div>
-        <Link to="/contact">Contact page</Link>
+      <div className={page}>
+        <h1>Hello page</h1>
+        <div>
+          <Link to="/about">About page</Link>
+        </div>
+        <div>
+          <Link to="/company/history">History page</Link>
+        </div>
+        <div>
+          <Link to="/contact">Contact page</Link>
+        </div>
+        <p>Lorem</p>
       </div>
     </Layout>
   )
