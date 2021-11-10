6965b5742c1b074dd08f89d0bc34a49de1651ec1
nguye374 <nguye374@sheridancollege.ca>
2021-11-10 03:01:53 -0500

    Nov 10 try out GraphQL

--- gatsby-config.js
+++ gatsby-config.js
@@ -6,6 +6,17 @@
 
 module.exports = {
   /* Your site config here */
+  siteMetadata: {
+    title: "The Recipes",
+    description: "Nice and clean recipes site",
+    author: "Viet",
+    person: { name: "viet", age: 29 },
+    simpleData: ["item 1", "item 2"],
+    complexData: [
+      { name: "viet", age: 29 },
+      { name: "sarah", age: 30 },
+    ],
+  },
   plugins: [
     `gatsby-plugin-styled-components`,
     `gatsby-plugin-image`,
