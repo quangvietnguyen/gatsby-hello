02c0dc98cb8dbd73a07c0275c0d553a5c74cc038
nguye374 <nguye374@sheridancollege.ca>
2021-11-10 02:13:18 -0500

    Nov 9 add cover pic and page title

--- src/pages/about.js
+++ src/pages/about.js
@@ -1,11 +1,38 @@
 import React from "react"
 import Layout from "../components/Layout"
 import styled from "styled-components"
+import { StaticImage } from "gatsby-plugin-image"
+import { Link } from "gatsby"
 
-export default function about() {
+export default function About() {
   return (
     <Layout>
-      <h1>About Page</h1>
+      <main className="page">
+        <section className="about-page">
+          <article>
+            <h2>Pop-up iceland meggings adaptogen.</h2>
+            <p>
+              Subway tile actually activated charcoal fanny pack edison bulb
+              swag semiotics flexitarian chartreuse mustache four loko hoodie
+              vape pour-over.
+            </p>
+            <p>
+              Fingerstache polaroid gochujang, salvia meggings food truck
+              affogato. Tilde kale chips try-hard, locavore glossier organic
+              literally yuccie.
+            </p>
+            <Link to="/contact" className="btn">
+              contact
+            </Link>
+          </article>
+          <StaticImage
+            src="../assets/images/about.jpeg"
+            alt="Person pouring salt into the bow"
+            className="about-img"
+            placeholder="blurred"
+          />
+        </section>
+      </main>
     </Layout>
   )
 }
--- src/pages/index.js
+++ src/pages/index.js
@@ -1,12 +1,26 @@
 import React from "react"
 import Layout from "../components/Layout"
-import Images from "../examples/Images"
-
+import { StaticImage } from "gatsby-plugin-image"
 export default function Home() {
   return (
     <Layout>
-      <h1>Home Page</h1>
-      <Images />
+      <main className="page">
+        <header className="hero">
+          <StaticImage
+            src="../assets/images/main.jpeg"
+            alt="cover photo"
+            className="hero-img"
+            placeholder="tracedSVG"
+            layout="fullWidth"
+          ></StaticImage>
+          <div className="hero-container">
+            <div className="hero-text">
+              <h1>The Recipes</h1>
+              <h4>no fluff, just recipes</h4>
+            </div>
+          </div>
+        </header>
+      </main>
     </Layout>
   )
 }
