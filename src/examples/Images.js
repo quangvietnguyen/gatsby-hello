22bff188f18aadfeacd74cf869d7c8d622bedc16
nguye374 <nguye374@sheridancollege.ca>
2021-11-10 01:44:55 -0500

    Nov 10 experiment with Image

--- src/examples/Images.js
+++ src/examples/Images.js
@@ -1,19 +1,63 @@
 import React from "react"
 import styled from "styled-components"
-import meal from "../assets/images/meals.jpeg"
 import { StaticImage } from "gatsby-plugin-image"
 
 export default function Images() {
   return (
     <Wrapper>
-      <StaticImage src="../assets/images/meals.jpeg" alt="spaghetti" />
-      <h2>Images</h2>
+      <article>
+        <h4>constrained / default</h4>
+        <StaticImage
+          src="../assets/images/recipe-1.jpeg"
+          alt="food"
+          placeholder="tracedSVG"
+          layout="constrained"
+          className="example-img"
+          as="section"
+        />
+      </article>
+      <article>
+        <h4>fixed</h4>
+        <StaticImage
+          src="../assets/images/recipe-1.jpeg"
+          alt="food"
+          placeholder="blurred"
+          layout="fixed"
+          width={200}
+          className="example-img"
+          as="div"
+        />
+      </article>
+      <article>
+        <h4>full width</h4>
+        <StaticImage
+          src="../assets/images/recipe-1.jpeg"
+          alt="food"
+          placeholder="dominantColor"
+          layout="fullWidth"
+          className="example-img"
+          as="article"
+        />
+      </article>
     </Wrapper>
   )
 }
 
 const Wrapper = styled.section`
-  img {
-    width: 200px;
+  width: 70vw;
+  margin: 0 auto;
+  display: grid;
+  text-align: center;
+  gap: 2rem;
+  article {
+    border: 2px solid red;
+  }
+  .example-img {
+    border-radius: 1rem;
+    height: 300px;
+  }
+  @media (min-width: 992px) {
+    grid-template-columns: 1fr 1fr 1fr;
+    height: 200px;
   }
 `
